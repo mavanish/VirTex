f5534e3bda0f893f1ec668e7ae4f9a167dc3c4f0
Avanish Mishra <avanish.iisc@gmail.com>
2022-10-06 14:40:49 -0600

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,10 +32,10 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-html_logo = "virtex.png"
+html_logo = "virtex_logo.png"
 html_theme_options = {
-    'logo_only': True,
-    'display_version': False,
+    'logo_only': False,
+    'display_version': True,
 }
 
 
