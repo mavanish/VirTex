fc50de01243ab61ca5f2a17664e80f7d306be0c1
Avanish Mishra <avanish.iisc@gmail.com>
2022-10-06 14:37:20 -0600

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,6 +11,7 @@ version = '0.1.0'
 
 # -- General configuration
 
+
 extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
@@ -30,6 +31,13 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_logo = "virtex.png"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
+
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
